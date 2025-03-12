d1c0499817e01542b3ba0e31117ef1fa734f9e80
Aanghell <andrei.anghell210@gmail.com>
2025-03-12 20:44:40 +0200

    finished 4th - reverse strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let reverseText=string.split("").reverse().join("");
+return reverseText;
 };
+
+// reverseString('hello world');
 
 // Do not edit below this line
 module.exports = reverseString;
+
+// npm test reverseString.spec.js
